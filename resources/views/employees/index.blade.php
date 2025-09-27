c2c092dfce1f81ab895cf9a03afc8d85c26a7d19
Jean Luc Kawel <20ka208@esisalama.org>
2025-09-27 20:29:18 +0200

    add entreprise

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -140,7 +140,14 @@
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                 </svg>
-                                                {{ $daysLeft }} jours restants
+                                                {{ $daysLeft }}
+                                                @php
+                                                    if ($daysLeft === 1) {
+                                                        echo 'day left';
+                                                    } else {
+                                                        echo 'days left';
+                                                    }
+                                                @endphp
                                             </p>
                                         @endif
                                     @endif
